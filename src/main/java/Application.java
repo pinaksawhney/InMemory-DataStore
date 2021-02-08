301f578d58a8b0cc9bc477ba4496af2cd9dc6b5d
psawhney <psawhney@vmware.com>
2021-02-07 21:50:01 -0500

    Adding constants

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,6 +9,14 @@ import org.example.parser.Parse;
 
 public class Application<T, U> {
 
+    private static final String SETV = "SET";
+    private static final String GET = "GET";
+    private static final String UNSET = "UNSET";
+    private static final String NUMEQUALTO = "NUMEQUALTO";
+    private static final String END = "END";
+
+
+
     private static BasicCommands<String, Integer> basicCommands = new BasicCommandsImpl<>();
     public static void main(String[] args) {
         String line;
@@ -29,23 +37,23 @@ public class Application<T, U> {
 
     private static void runCommand(Data<String, Integer> command) {
 
-        if(command.getCommand().equals("SET")) {
+        if(command.getCommand().equals(SETV)) {
             basicCommands.set(command.getKey(), Integer.parseInt(String.valueOf(command.getValue())));
         }
 
-        if(command.getCommand().equals("GET")) {
+        if(command.getCommand().equals(GET)) {
             System.out.println(basicCommands.get(command.getKey()));
         }
 
-        if(command.getCommand().equals("UNSET")) {
+        if(command.getCommand().equals(UNSET)) {
             basicCommands.unset(command.getKey());
         }
 
-        if(command.getCommand().equals("NUMEQUALTO")) {
+        if(command.getCommand().equals(NUMEQUALTO)) {
             System.out.println(basicCommands.numEqualTo(Integer.parseInt(String.valueOf(command.getValue()))));
         }
 
-        if(command.getCommand().equals("END")) {
+        if(command.getCommand().equals(END)) {
             basicCommands.end();
         }
     }
